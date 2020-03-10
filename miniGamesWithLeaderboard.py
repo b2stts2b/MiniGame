0661920b0193c8acd04c8e66a4bd9465054619cb
Axel Thunell <axelthunell1337@gmail.com>
2020-03-10 15:22:52 +0100

    Lagt in Spelet Gissa Tal. Kopplat highscorefunktionerna till Gissa Tal.

--- miniGamesWithLeaderboard.py
+++ miniGamesWithLeaderboard.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 filMapp = os.getcwd()
 textMap = f"{filMapp}\\Textfiler"
@@ -25,27 +26,93 @@ def readLeaderBoard(filePath):
 #Updaterar textfilen med nya highscore	
 def uploadLeaderBoard(scores, filePath):
 	with open(f"{textMap}\\{filePath}", "w") as f:
-		for player in scores:
-			f.write(f"{player}\n")
+		for i in range(len(scores)):
+			if i == len(scores)-1:
+				f.write(f"{scores[i]}")
+			else:
+				f.write(f"{scores[i]}\n")
 
 #Har spelaren fått ett highscore?
-def isHighScore(scores, number):
-	lowestScore = int(scores[-1].split(",")[1])
-	return (number > lowestScore)
+def isHighScore(scores, number, highIsGood):
+	lastScore = int(scores[-1].split(",")[1])
+	if highIsGood:
+		return number > lastScore
+	else:
+		return number < lastScore
 
 #Uppdatera highscore-listan
-def updateHighScore(scores, name, number):
+def updateHighScore(scores, name, number, highIsGood):
 	nameAndScore = f"{name},{number}"
-	for i in range(len(scores)-1, -1, -1):
-		score = int(scores[i].split(",")[1])
-		if number <= score:
-			for k in range(len(scores)-1, i + 2, -1):
+	if highIsGood:
+		#If our number is the best, slide worse scores and put ours at #1
+		if number > int(scores[0].split(",")[1]):
+			for k in range(len(scores)-1, 0, -1):
 				scores[k] = scores[k-1]
-			scores[i+1] = nameAndScore
-			break
+			scores[0] = nameAndScore
+		else:
+			#Loop from worst to best
+			for i in range(len(scores)-1, 0, -1):
+				score = int(scores[i].split(",")[1])
+			
+				#If our number is worse than score, slide worse scores one down and input number
+				if number <= score:
+					for k in range(len(scores)-1, i + 2, -1):
+						scores[k] = scores[k-1]
+					scores[i+1] = nameAndScore
+					break
+	else:
+		#If our number is the best, slide worse scores and put ours at #1
+		if number < int(scores[0].split(",")[1]):
+			for k in range(len(scores)-1, 0, -1):
+				scores[k] = scores[k-1]
+			scores[0] = nameAndScore
+		else:
+			#Loop from worst to best
+			for i in range(len(scores)-1, -1, -1):
+				score = int(scores[i].split(",")[1])
+			
+				#If our number is worse than or equal to score, slide worse scores one down and input number
+				if number >= score:
+					for k in range(len(scores)-1, i + 1, -1):
+						scores[k] = scores[k-1]
+					scores[i+1] = nameAndScore
+					break
+	return scores
 
 def GissaTal():
-	pass
+	os.system("cls")
+
+	rättTal = random.randint(1,1000000)
+	print(f"Jag tänker på ett tal mellan 1 och 1 miljon.({rättTal})")
+	
+	rättSvar = False
+	antalGissningar = 0
+	highScore = readLeaderBoard("leaderBoardGissaTal.txt")
+
+	while not rättSvar:
+		try:
+			svar = int(input("Gissa talet: "))
+			antalGissningar += 1
+			if svar == rättTal:
+				os.system("cls")
+				print(f"Du svarade rätt! Jag tänkte på talet {rättTal}")
+				print(f"Det tog dig {antalGissningar} försök.")
+				if isHighScore(highScore, antalGissningar, False):
+					print("Det är ett nytt highScore!")
+					name = input("Ditt namn: ")
+					highScore = updateHighScore(highScore, name, antalGissningar, False)
+					uploadLeaderBoard(highScore, "leaderBoardGissaTal.txt")
+				else:
+					print("Du slo")
+
+				break
+			elif svar < rättTal:
+				print("För lågt!")
+			else:
+				print("För högt!")
+		except ValueError:
+			print("Du kan bara gissa heltal, försök igen.")
+
 
 def SpaceInvaders():
 	pass
@@ -111,7 +178,10 @@ def LeaderBoard():
 	elif svar == "3":
 		LeaderBoardSurvival()
 
-	input("Enter...")
+	try:
+		input("Enter...")
+	except:
+		pass
 
 def Instruktion():
 	svar = ""
@@ -137,12 +207,12 @@ def Instruktion():
 	input("Enter...")
 
 
+#Här börjar applikationen
 while True:
 	svar = ""
 	#Fråga efter vad spelaren vill göra
 	while svar not in ["1","2","3","4"]:
 		os.system("cls")
-		print(f"{os.getcwd()}")
 		print("Vad vill du göra?")
 		print("1. Spela.")
 		print("2. LeaderBoard.")
